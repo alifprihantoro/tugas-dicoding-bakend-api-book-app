515dac11e5b99a30d88b4334b7131b9126d69500
alifprihantoro <alifmuryp@gmail.com>
2023-02-09 09:56:49 +0700

    feat: add happy framwork, delete not use and linter config

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'prefer-template': ['error'],
+    'no-multi-spaces': ['error', { ignoreEOLComments: false }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-trailing-spaces': ['error'],
+    'no-mixed-spaces-and-tabs': ['error'],
+    'camelcase': ['error'],
+    'indent': ['error', 2],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'never',
+    ],
+    'no-console': ['warn'],
+    'no-alert': ['warn'],
+  },
+}
